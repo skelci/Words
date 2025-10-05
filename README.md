f2e0aafe2db7ff2d7a177b316d78b7bf6716961b
Skelci <nikomaren2@gmail.com>
2025-10-05 15:27:19 +0200

    made some kind of terminal GUI

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 ## Description
 
-Simple program for learning english words.
+Simple program for learning new words.
 
 ## Usage
 
 Put words in `res/words.txt` file in format `word-translation` and run `python src/main.py`.
 
 To exit program type `;`.
-To print statistics type `,`.
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import random
 from enum import IntEnum
 
@@ -8,6 +9,38 @@ class Lang(IntEnum):
     ENG = 0
     SLO = 1
 
+ANSI_CLS = "\x1b[2J\x1b[3J\x1b[H"
+ANSI_GOTO_UP = lambda n: f"\x1b[{n}A"
+ANSI_GOTO_COL = lambda x: f"\x1b[{x}G"
+ANSI_GOTO = lambda x, y: f"\x1b[{y};{x}H"
+
+ANSI_BG_RED = "\x1b[41m"
+ANSI_BG_GREEN = "\x1b[42m"
+ANSI_BG_BLACK = "\x1b[40m"
+ANSI_BG_RESET = "\x1b[49m"
+
+ANSI_TEXT_RED = "\x1b[31m"
+ANSI_TEXT_GREEN = "\x1b[32m"
+ANSI_TEXT_WHITE = "\x1b[97m"
+ANSI_TEXT_GRAY = "\x1b[90m"
+
+# Unicode box parts
+UNCB = {
+    'TLD': '╔',
+    'TRD': '╗',
+    'BLD': '╚',
+    'BRD': '╝',
+    'HD':  '═',
+    'VD':  '║',
+    'TL':  '┌',
+    'TR':  '┐',
+    'BL':  '└',
+    'BR':  '┘',
+    'H':   '─',
+    'V':   '│',
+    'TJ':  '┬',
+    'BJ':  '┴',
+}
 
 
 class Words:
@@ -83,13 +116,13 @@ class Words:
                     return self.__words_slo[word], Lang.SLO
             
 
-    def check_word(self, word, anwser, original_lang):
+    def check_word(self, word, answer, original_lang):
         if original_lang == Lang.ENG:
             word_eng = word
-            word_slo = anwser
+            word_slo = answer
             word_key = word_eng
         else:
-            word_eng = anwser
+            word_eng = answer
             word_slo = word
             word_key = self.__words_eng[word_slo]
         correct = False
@@ -105,10 +138,29 @@ class Words:
 
         self.__cache[word_key] /= self.learning_factor if correct else 1 / self.learning_factor
 
-        print('Correct!' if correct else 'Wrong!')
+        success_message = 'Correct!' if correct else 'Wrong!'
+        learned_message = f'Learned: {1 / self.__cache[word_key]}'
         if not correct:
-            print(f'Correct answer: {self.__words_eng[word_slo] if original_lang == Lang.SLO else self.__words_slo[word_eng]}')
-        print(f'Learned: {1 / self.__cache[word_key]}')
+            guessing_word_message = f'Guessing word: {word}'
+            correct_ans_message = f'Correct answer: {self.__words_eng[word_slo] if original_lang == Lang.SLO else self.__words_slo[word_eng]}'
+            print(ANSI_BG_RED)
+        else:
+            print(ANSI_BG_GREEN)
+
+        print(ANSI_GOTO(1, 14))
+        print(UNCB['TL'] + UNCB['H'] * 72 + UNCB['TR'])
+        print(f'{UNCB["V"]} {success_message:<70} {UNCB["V"]}')
+        if not correct:   
+            print(f'{UNCB["V"]} {guessing_word_message:<70} {UNCB["V"]}')
+            print(f'{UNCB["V"]} {f"Your answer: {answer}":<70} {UNCB["V"]}')
+            print(f'{UNCB["V"]} {correct_ans_message:<70} {UNCB["V"]}')
+        print(f'{UNCB["V"]} {learned_message:<70} {UNCB["V"]}')
+        print(UNCB['BL'] + UNCB['H'] * 72 + UNCB['BR'])
+        print(ANSI_BG_RESET, end='')
+        if correct:
+            print(" " * 74)
+            print(" " * 74)
+            print(" " * 74)
 
     def print_stats(self):
         learned = sum((1 / item for item in self.__cache.values()))
@@ -116,39 +168,73 @@ class Words:
         avg_learned = learned / len(self.__cache)
         min_learned = min(self.__cache.values())
         max_learned = max(self.__cache.values())
-        print(f'Learned: {learned}')
-        print(f'Avg learned: {avg_learned}')
-        print(f'Min learned: {1 / max_learned}')
-        print(f'Max learned: {1 / min_learned}')
-        print(f'Words to learn: {len(self.__cache) - learned_sum}')
+
+        STAT_OFFSET_X = 20
+
+        def get_formated_column(stat_name, stat_value):
+            return f'{" " * STAT_OFFSET_X}{UNCB["VD"]} {stat_name:<20}{round(stat_value, 4):>10} {UNCB["VD"]}'
+        
+        print(" " * STAT_OFFSET_X + UNCB['TLD'] + UNCB['HD'] * 32 + UNCB['TRD'])
+        print(get_formated_column('Learned', learned))
+        print(get_formated_column('Avg learned', avg_learned))
+        print(get_formated_column('Min learned', 1 / max_learned))
+        print(get_formated_column('Max learned', 1 / min_learned))
+        print(get_formated_column('Words to learn', len(self.__cache) - learned_sum))
+        print(get_formated_column('Total words', len(self.__cache)))
+        print(" " * STAT_OFFSET_X + UNCB['BLD'] + UNCB['HD'] * 32 + UNCB['BRD'])
 
 
 
 def main():
     wordlearn = Words('res/words.txt', 'res/cache.txt')
+    running = True
 
-    def print_stats():
-        print("\n------------------------------------------------\n")
-        wordlearn.print_stats()
-        print("\n------------------------------------------------\n")
-
-    while True:
+    def ask_word():
         word, lang = wordlearn.get_random_word()
-        print(f'Word: {word} ({"ENG" if lang == Lang.ENG else "SLO"})')
-        answer = input('Answer: ')
-        if answer == ',':
-            print_stats()
-            answer = input('Answer: ')
-            wordlearn.check_word(word, answer, lang)
-            continue
-        if answer == ';':
-            answer = input('Answer: ')
-            wordlearn.check_word(word, answer, lang)
-            break
+        answer = get_answer(word, lang)
         wordlearn.check_word(word, answer, lang)
-        print("------------------\n")
+        
 
-    print_stats()
+    def get_answer(word, lang):
+        lang_str = 'ENG' if lang == Lang.ENG else 'SLO'
+
+        print(ANSI_GOTO(1, 10))
+        print(UNCB['TL'] + UNCB['H'] * 73 + UNCB['TR'])
+        print(f'{UNCB["V"]} {ANSI_TEXT_GRAY}Word to guess: {ANSI_TEXT_WHITE}{word:<50} {ANSI_TEXT_GRAY}({lang_str}) {ANSI_TEXT_WHITE}{UNCB["V"]}')
+        print(f'{UNCB["V"]} {ANSI_TEXT_GRAY}Answer: {ANSI_TEXT_WHITE}{" " * 63} {UNCB["V"]}')
+        print(UNCB['BL'] + UNCB['H'] * 73 + UNCB['BR'], end='')
+        print(ANSI_GOTO_UP(1)+ANSI_GOTO_COL(11), end='')
+
+        answer = input()
+        if answer == ';':
+            print(ANSI_GOTO(1, 22) + ANSI_TEXT_GRAY + 'Pending exit...' + ANSI_TEXT_WHITE + ' ' * 40, end='')
+            answer = get_answer(word, lang)
+            nonlocal running
+            running = False
+        return answer
+
+    print(ANSI_CLS)
+    while True:
+        cols, rows = shutil.get_terminal_size(fallback=(80, 22))
+        if cols < 80 or rows < 22:
+            print('Terminal size is too small. Please resize to at least 80x22 and press Enter.')
+            print(f'Current size: {cols}x{rows}')
+            input()
+            print(ANSI_CLS)
+        else:
+            break
+
+    print(ANSI_GOTO(1, 22) + ANSI_TEXT_GRAY + 'Type ";" as your answer to exit.' + ANSI_TEXT_WHITE, end='')
+
+    while running:
+        print(ANSI_GOTO(1, 1))
+        wordlearn.print_stats()
+        print('\n')
+        ask_word()
+
+    print(ANSI_GOTO(1, 1))
+    wordlearn.print_stats()
+    print(ANSI_GOTO(1, 21))
 
 
 if __name__ == '__main__':
